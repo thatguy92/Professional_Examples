3efa94fa5c77b10972c6599e563b54a14278897d
thatguy92 <34484614+thatguy92@users.noreply.github.com>
2022-05-17 20:09:43 -0500

    Create Month_Scripts_GitHub.py

---
+++ Month_Scripts_GitHub.py
@@ -0,0 +1,18 @@
+'''
+This script simply pulls the defined functions from the three listed scripts, so that I only have to run one
+script every month instead of three separate ones.
+'''
+
+from Month_Stacker_Pull_GitHub import month_stacker_pull
+from Month_Shared_Wallets_GitHub import month_shared_wallets
+from Month_HundredK_Customers_GitHub import month_100k
+
+
+def three_scripts():
+    month_stacker_pull()
+    month_shared_wallets()
+    month_100k()
+
+
+if __name__ == '__main__':
+    three_scripts()
