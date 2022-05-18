b0ccd138efc3cad7edb708893a3142dc5071c577
thatguy92 <34484614+thatguy92@users.noreply.github.com>
2022-05-17 20:49:35 -0500

    Update Month_Scripts_GitHub.py

--- Month_Scripts_GitHub.py
+++ Month_Scripts_GitHub.py
@@ -5,13 +5,13 @@ script every month instead of three separate ones.
 
 from Month_Stacker_Pull_GitHub import month_stacker_pull
 from Month_Shared_Wallets_GitHub import month_shared_wallets
-from Month_High_Volume_GitHub import month_100k
+from Month_High_Volume_GitHub import month_high_volume
 
 
 def three_scripts():
     month_stacker_pull()
     month_shared_wallets()
-    month_100k()
+    month_high_volume()
 
 
 if __name__ == '__main__':
